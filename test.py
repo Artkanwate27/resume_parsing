c819125f8239ac16ad46fa14b8b7d875b5a3603b
arti kanwate <143333213+Artkanwate27@users.noreply.github.com>
2025-08-13 11:58:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import streamlit as st
+import numpy as np
+import os
+import shutil
 
 opt_select = st.selectbox(
     "Please select from one of the options",
-    ("Upload", "Abs", "Bgf")
+    ("Abs", "Bgf")
 )
 
-if opt_select == "Upload":
+if opt_select!="Upload":
     with st.form("upload-form", clear_on_submit=True):
-        st.write("Please upload a text file.")
+        st.write("New created.")
         uploaded_file = st.file_uploader("Upload text file", type=["txt"])
-        submitted = st.form_submit_button("Submit")
-        
-        if submitted:
-            if uploaded_file is not None:
-                st.success("File uploaded successfully!")
-            else:
-                st.warning("No file selected. Please upload a text file.")
-else:
-    st.write(f"You selected option: {opt_select}")
+        submitted= st.form_submit_button()
+        print("AAA",uploaded_file)
+        if uploaded_file is not None:
+            st.write("Success.")
+
+        else:
+            st.warning("No Selection")
+
